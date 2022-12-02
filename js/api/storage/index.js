6a527ba4310dc50a92bc2d185295ea40b77637d4
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2022-12-02 12:32:59 +0100

    made save, load, remove, and clear from local storage

---
+++ js/api/storage/index.js
@@ -0,0 +1,11 @@
+import { save } from "./save";
+import { remove } from "./remove";
+import { clear } from "./clear";
+import { load } from "./load";
+
+export default {
+    save,
+    remove,
+    clear,
+    load,
+}
